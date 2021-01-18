2a22b28d97a7f24bae00f01935dfb095a1ca8f0a
Everest <caioeverest.b@gmail.com>
2021-01-18 19:24:49 -0300

    improve responses

--- src/interceptor/languageStrings.js
+++ src/interceptor/languageStrings.js
@@ -1,15 +1,16 @@
 module.exports = {
   'pt-BR': {
     translation: {
-      WELCOME_MSG: 'Fala jovem, tudo certo? Me diz ai, qual vai ser?',
+      WELCOME_MSG: 'Fala jovem, tudo certo? Me diz aí, qual vai ser?',
       READING_MARKS: 'No dia %s, você marcou %s',
-      EMPTY_MARKS: 'No dia %s não tem marcações',
+      EMPTY_MARKS: 'No dia %s você não tem marcações',
       CONFIRMATION_MSG: 'Posso marcar a %s, então?',
       GOODBYE_MARKED_MSG: 'Show meu guri, ponto marcado!',
       GOODBYE_NOT_MARKED_MSG: 'Opa, deixa quieto então, bobo!',
-      REFLECTOR_MSG: 'Não olha na minha cara, você me deixou engatilhada e iniciou a %s',
+      CANCEL_MSG: 'Não olha na minha cara, você me deixou engatilháda',
       FALLBACK_MSG: 'Que papo é esse meu irmão.',
-      ERROR_MSG: 'Vish, a API dos caras ta com problema, retornou %s. Vê os logs.',
+      HELP: 'Então menó, tu pode me pedir pra marcar o ponto ou te falar as marcações do dia',
+      ERROR_MSG: 'Vish, a A.P.I. dos caras tá com problema, retornou %s. Vê os lógs.',
       TODAY: 'hoje',
       IN: 'entrada',
       OUT: 'saida',
